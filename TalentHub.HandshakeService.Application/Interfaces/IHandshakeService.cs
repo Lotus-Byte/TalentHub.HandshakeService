ab44ea6f89aaf7eadcf472243f77d049ee3cc6d2
andreibaksheev <154066928+andreibaksheev@users.noreply.github.com>
2025-02-25 09:03:41 +0300

    Update IHandshakeService.cs

--- TalentHub.HandshakeService.Application/Interfaces/IHandshakeService.cs
+++ TalentHub.HandshakeService.Application/Interfaces/IHandshakeService.cs
@@ -6,6 +6,7 @@ public interface IHandshakeService
 {
     // TODO: CancellationToken
     Task<bool> SendHandshakeAsync(SendHandshakeDto sendHandshakeDto);
+    // TODO: лучше возвращать IReadOnlyCollection
     Task<HandshakeDto[]?> GetHandshakesBySenderAsync(Guid fromUserId);
     Task<HandshakeDto[]?> GetHandshakesByRecipientAsync(Guid fromUserId);
     Task<bool> DeleteHandshakeAsync(Guid handshakeId);
