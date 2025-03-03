72fd3efb8612aff40de958ff816662bd416999d9
Pavel <test21299@gmail.com>
2025-03-03 20:52:39 +0300

    Dockerfile change

--- TalentHub.HandshakeService.Api/Program.cs
+++ TalentHub.HandshakeService.Api/Program.cs
@@ -4,14 +4,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TalentHub.HandshakeService.Api.Configurations;
 using TalentHub.HandshakeService.Api.Extensions;
 using TalentHub.HandshakeService.Application.Interfaces;
 using TalentHub.HandshakeService.Application.Services;
 using TalentHub.HandshakeService.Infrastructure.Abstractions;
+using TalentHub.HandshakeService.Infrastructure.Abstractions.DomainEvents;
 using TalentHub.HandshakeService.Infrastructure.Abstractions.Repositories;
 using TalentHub.HandshakeService.Infrastructure.Data;
+using TalentHub.HandshakeService.Infrastructure.EventHandlers;
 using TalentHub.HandshakeService.Infrastructure.Models.Notification;
 using TalentHub.HandshakeService.Infrastructure.Providers;
 using TalentHub.HandshakeService.Infrastructure.Repositories;
@@ -70,12 +73,23 @@ builder.Services.AddScoped<IHandshakeRepository, HandshakeRepository>();
 builder.Services.AddScoped<IHandshakeService, HandshakeService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<INotificationEventFactory, NotificationEventFactory>();
+builder.Services.AddScoped<IEventHandler<NotificationEvent>, NotificationEventHandler>();
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<HandshakeDbContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    
+    logger.LogInformation("Applying migrations...");
+    dbContext.Database.Migrate();
+    logger.LogInformation("Migrations applied successfully.");
+}
 
 if (app.Environment.IsDevelopment())
 {
---
+++ TalentHub.HandshakeService.Infrastructure/EventHandlers/NotificationEventHandler.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using TalentHub.HandshakeService.Infrastructure.Abstractions.DomainEvents;
+using TalentHub.HandshakeService.Infrastructure.Models.Notification;
+
+namespace TalentHub.HandshakeService.Infrastructure.EventHandlers;
+
+public class NotificationEventHandler : IEventHandler<NotificationEvent>
+{
+    private readonly IBus _bus;
+    private readonly ILogger<NotificationEventHandler> _logger;
+
+    public NotificationEventHandler(IBus bus, ILogger<NotificationEventHandler> logger)
+    {
+        _bus = bus;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(NotificationEvent notificationEvent)
+    {
+         var json = JsonSerializer.Serialize(notificationEvent);
+
+        await _bus.Publish(notificationEvent);
+
+        _logger.LogInformation($"""
+                                Notification event published:
+                                 {json}
+                                """);
+    }
+}
